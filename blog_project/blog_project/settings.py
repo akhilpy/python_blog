72433d20168f30189f95574022dfaff531d13764
akhilpy <akhilendra44@gmail.com>
2018-11-16 11:18:42 +0530

    added media root and profile section

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog.apps.BlogConfig'
+    #apps
+    'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -130,3 +133,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK= 'bootstrap4'
+
+MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL='/media/'
+
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
--- blog_project/users/views.py
+++ blog_project/users/views.py
@@ -1,17 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 def register(request):
     if request.method== 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
 
         if form.is_valid():
+            form.save()
             username= form.cleaned_data.get('username')
-            messages.success("User created successfully")
+            messages.success(request, f'User {username} created successfully')
 
+            return  redirect('blog-home')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request,'users/register.html', {'form':form})
 
-
+@login_required()
+def profile(request):
+    return render(request, 'users/profile.html')
